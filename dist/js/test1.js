3e5af309275493ebe62027e6bbd228a50db0ebef
Manuel Francisco <ciesafrancisco@hotmail.com>
2024-07-25 10:09:41 +0200

    media queries

---
+++ dist/js/test1.js
@@ -0,0 +1,7 @@
+// Function to be exported from test1.js
+export function greet() {
+  console.log('Hello from test1.js');
+}
+
+// Variable to be exported from test1.js
+export const name = 'Module Test1';
